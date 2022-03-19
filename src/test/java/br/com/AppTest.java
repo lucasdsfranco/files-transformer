f7042cdfb7048da62a49a1d0eb592b16978de2df
lucasdsfranco <41924810+lucasdsfranco@users.noreply.github.com>
2022-03-18 22:46:15 -0300

    initial commit

---
+++ src/test/java/br/com/AppTest.java
@@ -0,0 +1,14 @@
+package br.com;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+public class AppTest 
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
